a1c6370439ab626cdcd0b0212d6579a9616c2f12
Nihal Metin <nihalmetin1908@gmail.com>
2025-12-31 23:52:22 +0300

    Refactor code structure for improved readability and maintainability

--- app.py
+++ app.py
@@ -8,3 +8,13 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
+
+load_dotenv()
+
+st.title("CSV Document Q&A with Google Generative AI")
+
+loader = CSVLoader("MovieDatabase.csv")
+data = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+doc

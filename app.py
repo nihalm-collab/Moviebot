190d66fe7e32eb283973bc6ec3f27daadac689aa
Nihal Metin <nihalmetin1908@gmail.com>
2026-01-01 15:50:31 +0300

    Update .env with new Google API key; modify app.py and gemini_basic_rag.ipynb for movie recommendation functionality

--- app.py
+++ app.py
@@ -6,15 +6,53 @@ from dotenv import load_dotenv
 from langchain_chroma import Chroma
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain
+
 
 load_dotenv()
 
-st.title("CSV Document Q&A with Google Generative AI")
+st.title("Movie Dictionary with Google Generative AI")
 
-loader = CSVLoader("MovieDatabase.csv")
+loader = CSVLoader("IMDB_Top_1000_Movies_Dataset.csv", encoding="utf-8")
 data = loader.load()
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-doc
+docs = text_splitter.split_documents(data)
+
+embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004", task_batch_size=100)
+vector_store = Chroma.from_documents(documents=docs, embedding=embeddings)
+
+retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 10 })
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",  
+    temperature=0.3,  
+    max_tokens=500,
+)
+
+query = st.text_input("Enter your question about movies:")
+prompt = query
+
+system_prompt = (
+    "You are a helpful movie recommendation assistant. Use the following movie database context to answer the user's question."
+    "Only recommend movies from the provided context"
+    "Mention IMDB ratings and year where relevant."
+    "If you don't find suitable movies in the context, say so honestly."
+    "\n\n"
+    "{context}"
+)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("user", "{input}"),
+    ]
+)
+
+if query:
+    question_answering_chain = create_stuff_documents_chain(llm, prompt)
+    rag_chain = create_retrieval_chain(retriever, question_answering_chain)
+    response = rag_chain.invoke({"input": prompt.format_messages(input=prompt).to_string()})
+
+    st.write(response["answer"])
